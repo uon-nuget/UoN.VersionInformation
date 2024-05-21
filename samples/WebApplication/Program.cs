e7fb93768f5392e2458166c2c4a8a8391419f08e
Jonathan Couldridge <jcouldridge@outlook.com>
2024-05-21 12:05:08 +0100

    endpoint routing

--- samples/WebApplication/Program.cs
+++ samples/WebApplication/Program.cs
@@ -2,37 +2,58 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
-var useEndpointRouting = false; // Whether to use modern endpoint routing or terminal middleware
+var useEndpointRouting = true; // Whether to use modern endpoint routing or terminal middleware
 
 // TODO DI samples
+
+var objectSource = new
+{
+    Version = "v1.0.0",
+    Description = "You know, for versions."
+};
 
 // Pre-endpoint routing terminal middleware
 if (!useEndpointRouting)
 {
     // Map to the default path `/version`
     // Note that ORDER MATTERS as we are mapping terminally at the same route
-    var objectSource = new
-    {
-        Version = "v1.0.0",
-        Description = "You know, for versions."
-    };
 
-    app.UseUonVersion(objectSource);
+    app.UseUonVersionInformation(objectSource);
 
     // named arguments avoid ambiguity
     // if using a `string` source at the default route
-    app.UseUonVersion(source: "v1.0.0");
+    app.UseUonVersionInformation(source: "v1.0.0");
 
-    app.UseUonVersion(); // Map Assembly version by default
+    app.UseUonVersionInformation(); // Map Assembly version by default
 
 
     // Map to custom paths - note that these all work concurrently
-    app.UseUonVersion("/custompath"); // Map Assembly version to `/version`
-    app.UseUonVersion("/customstring", "Version 1.0");
-    app.UseUonVersion("/customobject", objectSource);
+    app.UseUonVersionInformation("/custompath"); // Map Assembly version to `/version`
+    app.UseUonVersionInformation("/customstring", "Version 1.0");
+    app.UseUonVersionInformation("/customobject", objectSource);
 }
 
 app.UseRouting(); // We Explicitly opt into endpoint routing here; you may not need to
+
+// Endpoint routing
+if (useEndpointRouting)
+{
+    // Map to the default path `/version`
+    // Note that it is not possible to map multiple times to the same endpoint;
+    // These will error unless only one is present!
+    app.MapUonVersionInformation();
+    
+    app.MapUonVersionInformation(objectSource);
+
+    // named arguments avoid ambiguity
+    // if using a `string` source at the default route
+    app.MapUonVersionInformation(source: "v1.0.0");
+    
+    // Map to custom paths - note that these all work concurrently
+    app.MapUonVersionInformation("/custompath"); // Map Assembly version to `/version`
+    app.MapUonVersionInformation("/customstring", "Version 1.0");
+    app.MapUonVersionInformation("/customobject", objectSource);
+}
 
 app.MapGet("/", () => "Hello World!");
 
--- src/UoN.AspNetCore.VersionMiddleware/BuilderExtensions/UonVersionInformationApplicationBuilderExtensions.cs
+++ src/UoN.AspNetCore.VersionMiddleware/BuilderExtensions/UonVersionInformationApplicationBuilderExtensions.cs
@@ -11,6 +11,8 @@ namespace Microsoft.AspNetCore.Builder
     /// </summary>
     public static class UonVersionInformationApplicationBuilderExtensions
     {
+        private const string DefaultPath = "/version";
+
         /// <summary>
         /// Adds a custom endpoint to the pipeline,
         /// responding only to GET requests with the output
@@ -24,7 +26,7 @@ namespace Microsoft.AspNetCore.Builder
         /// Defaults to Entry Assembly Informational Version.
         /// </param>
         /// <returns>The `IApplicationBuilder` instance</returns>
-        public static IApplicationBuilder UseUonVersion(
+        public static IApplicationBuilder UseUonVersionInformation(
             this IApplicationBuilder app,
             PathString path,
             object? source = null)
@@ -34,19 +36,7 @@ namespace Microsoft.AspNetCore.Builder
             // Default to entry assembly
             source ??= Assembly.GetEntryAssembly();
 
-            // switch on what we've been passed
-            // to choose appropriate shortcut behaviours
-            return source switch
-            {
-                // If we've been passed an assembly, explicitly use an assembly provider
-                // This is a good safety in case Assembly Type Handlers have been cleared
-                // from the configured service.
-                Assembly a => app.Map(path,
-                    appBuilder =>
-                        appBuilder.UseMiddleware<VersionMiddleware>(new AssemblyInformationalVersionProvider(a))),
-                _ => app.Map(path,
-                    appBuilder => appBuilder.UseMiddleware<VersionMiddleware>(source ?? Array.Empty<object>()))
-            };
+            return app.Map(path, b => b.UseUonVersionInformationCore(source));
         }
 
         /// <summary>
@@ -61,10 +51,25 @@ namespace Microsoft.AspNetCore.Builder
         /// Defaults to Entry Assembly Informational Version.
         /// </param>
         /// <returns>The `IApplicationBuilder` instance</returns>
-        public static IApplicationBuilder UseUonVersion(
+        public static IApplicationBuilder UseUonVersionInformation(
             this IApplicationBuilder app,
             object? source = null)
-            => app.UseUonVersion("/version", source)
+            => app.UseUonVersionInformation(DefaultPath, source)
                ?? throw new ArgumentNullException(nameof(app));
+
+        internal static IApplicationBuilder UseUonVersionInformationCore(this IApplicationBuilder app, object? source = null)
+        {
+            // switch on what we've been passed
+            // to choose appropriate shortcut behaviours
+            return source switch
+            {
+                // If we've been passed an assembly, explicitly use an assembly provider
+                // This is a good safety in case Assembly Type Handlers have been cleared
+                // from the configured service.
+                Assembly a =>
+                    app.UseMiddleware<VersionMiddleware>(new AssemblyInformationalVersionProvider(a)),
+                _ => app.UseMiddleware<VersionMiddleware>(source ?? Array.Empty<object>())
+            };
+        }
     }
 }
